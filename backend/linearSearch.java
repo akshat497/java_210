22b6a3cad83a14ea5ee1d471db186304c88effe8
Akshat Saini <akshatsaini497@gmail.com>
2026-09-09 12:58:57 +0530

    classes static final

--- backend/linearSearch.java
+++ backend/linearSearch.java
@@ -1,54 +1,85 @@
 public class linearSearch {
     public static void main(String[] args) {
+        class Student {
 
+            void display(String name, int age) {
+                System.out.println("Hello" + " " + name + " " + "your age is" + " " + age);
+
+            }
+
+            void Show(String name, int age) {
+                this.display(name, age);
+            }
+        }
+        Student obj1 = new Student();
+        Student obj2 = new Student();
+        obj1.Show("Akshat", 21);
+        obj2.Show("Sourav", 25);
         // int arr[]={11,12,13,14,15,16,17,18,19,20};
         // int left=0;
         // int right=arr.length-1;
         // int searched=16;
 
         // while(left<=right){
-        //     int mid=(left+right)/2;
+        // int mid=(left+right)/2;
 
-        //     if (arr[mid]== searched){
-        //         System.out.println("Element found at index: "+mid);
-        //         return;
+        // if (arr[mid]== searched){
+        // System.out.println("Element found at index: "+mid);
+        // return;
 
-        //     }else if(arr[mid]<searched){
-        //         left=mid+1;
-        //     }else{
-        //         right=mid-1;
-        //     }
+        // }else if(arr[mid]<searched){
+        // left=mid+1;
+        // }else{
+        // right=mid-1;
         // }
-        // what is oops ? 
-        // oops is object oriented programming system 
+        // }
+        // what is oops ?
+        // oops is object oriented programming system
         // 1 encapsulation
         // 2 inheritance
         // 3 polymorphism
         // 4 abstraction
-        class Human{
-            String name;
-            int age;
-            String gender;
+        // class Human{
+        // String name;
+        // int age;
+        // String gender;
+        // Human(String name, int age, String gender){
+        // this.name=name;
+        // this.age=age;
+        // this.gender=gender;
+        // }//constructor is called autmocatically at the time of object creation
+        // // Human(){
+        // // System.out.println("constructor called automatically");
+        // // }//constructor is a special method which is called automatically when an
+        // object is created
+        // // void CreateHuman(String nameFromObj, int ageFromObj, String
+        // genderFromObj){
+        // // this.name=nameFromObj;
+        // // this.age=ageFromObj;
+        // // this.gender=genderFromObj;
+        // // } //method
 
-            void CreateHuman(String nameFromObj, int ageFromObj, String genderFromObj){
-                this.name=nameFromObj;
-                this.age=ageFromObj;
-                this.gender=genderFromObj;
-            }
-            
-        }
-        Human obj1=new Human();
-        Human obj2=new Human();
-        obj1.CreateHuman("Akshat",21,"Male");
-        obj2.CreateHuman("John",25,"Male");
+        // }
+        // Human obj1=new Human("Akshat",21,"Male");
+        // Human obj2=new Human("sourav",21,"Male");
+        // System.out.println("Name: "+obj1.name);
+        // System.out.println("Age: "+obj1.age);
+        // System.out.println("Gender: "+obj1.gender);
+        // System.out.println("Name: "+obj2.name);
+        // System.out.println("Age: "+obj2.age);
+        // System.out.println("Gender: "+obj2.gender);
+        // Human obj1=new Human();
+        // Human obj2=new Human();
+        // obj1.CreateHuman("Akshat",21,"Male");
+        // obj2.CreateHuman("John",25,"Male");
 
-        System.out.println("Name: "+obj1.name);
-        System.out.println("Age: "+obj1.age);
-        System.out.println("Gender: "+obj1.gender);
+        // System.out.println("Name: "+obj1.name);
+        // System.out.println("Age: "+obj1.age);
+        // System.out.println("Gender: "+obj1.gender);
 
-        System.out.println("Name: "+obj2.name);
-        System.out.println("Age: "+obj2.age);
-        System.out.println("Gender: "+obj2.gender);
+        // System.out.println("Name: "+obj2.name);
+        // System.out.println("Age: "+obj2.age);
+        // System.out.println("Gender: "+obj2.gender);
         // a class is a blueprint of object
         // an object is an instance of class
 
@@ -56,19 +87,19 @@ public class linearSearch {
         // int searched=6;
         // int count=0;
         // for(int i=0; i<arr.length;i++){
-        //     if(arr[i]==searched){
-        //         System.out.println("Element found at index: "+i);
-               
-        //     }else{
-        //         count++;
-        //     }
+        // if(arr[i]==searched){
+        // System.out.println("Element found at index: "+i);
+
+        // }else{
+        // count++;
+        // }
         // }
 
         // if(count==arr.length){
-        //     System.out.println("Element not found");
+        // System.out.println("Element not found");
         // }
     }
-    // binery search 
-    // array must be sorted in ascending order 
+    // binery search
+    // array must be sorted in ascending order
 
 }
